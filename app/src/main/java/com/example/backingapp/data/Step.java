e1fd0e961b01bcc76505b5b884a0860e5c52f92b
Kareem Farag <kareemfarag@hotmail.com>
2020-11-02 01:41:27 +0200

    Feat Adding Step Data Model Class

--- app/src/main/java/com/example/backingapp/data/Step.java
+++ app/src/main/java/com/example/backingapp/data/Step.java
@@ -1,4 +1,58 @@
 package com.example.backingapp.data;
 
 public class Step {
+    private Integer id;
+    private String shortDescription;
+    private String description;
+    private String videoUrl;
+    private String thumbnailUrl;
+
+    public Step(Integer id, String shortDescription, String description, String videoUrl, String thumbnailUrl) {
+
+        this.id = id;
+        this.shortDescription = shortDescription;
+        this.description = description;
+        this.videoUrl = videoUrl;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
 }
