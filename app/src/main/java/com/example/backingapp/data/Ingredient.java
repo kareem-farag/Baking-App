239283ae7ab7901d00d58d6c1bf9c8698c6c8de4
Kareem Farag <kareemfarag@hotmail.com>
2020-11-02 01:37:49 +0200

    Feat Adding Ingredient Data Model Class

--- app/src/main/java/com/example/backingapp/data/Ingredient.java
+++ app/src/main/java/com/example/backingapp/data/Ingredient.java
@@ -1,4 +1,37 @@
 package com.example.backingapp.data;
 
 public class Ingredient {
+    private String quentity;
+    private String measure;
+    private String imagle;
+
+    public Ingredient(String quentity, String measure, String imagle) {
+        this.quentity = quentity;
+        this.measure = measure;
+        this.imagle = imagle;
+    }
+
+    public String getQuentity() {
+        return quentity;
+    }
+
+    public void setQuentity(String quentity) {
+        this.quentity = quentity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getImagle() {
+        return imagle;
+    }
+
+    public void setImagle(String imagle) {
+        this.imagle = imagle;
+    }
 }
