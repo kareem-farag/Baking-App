83bf0092be95f147d3ef6aa0f8750dc3edf8f2b5
Kareem Farag <kareemfarag@hotmail.com>
2020-11-01 14:59:21 +0200

    Refactor Changing Recipe Main Activity gridview to recycler view

--- app/src/main/java/com/example/backingapp/recyclers/RecipeAdapter.java
+++ app/src/main/java/com/example/backingapp/recyclers/RecipeAdapter.java
@@ -1,10 +1,11 @@
 package com.example.backingapp.recyclers;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.backingapp.R;
@@ -12,24 +13,28 @@ import com.example.backingapp.utils.Recipe;
 
 import java.util.List;
 
-public class RecipeAdapter extends BaseAdapter {
-    List <Recipe> recipeList ;
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+    List<Recipe> recipeList;
     Context context;
 
 
-    public RecipeAdapter (Context context , List<Recipe> recipesIds) {
-        this.recipeList = recipesIds ;
+    public RecipeAdapter(Context context, List<Recipe> recipesIds) {
+        this.recipeList = recipesIds;
         this.context = context;
 
     }
+
+    @NonNull
     @Override
-    public int getCount() {
-        return recipeList.size();
+    public RecipeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.recipe_holder, viewGroup, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public Object getItem(int position) {
-        return recipeList.get(position);
+    public void onBindViewHolder(@NonNull RecipeAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.recipeTitle.setText(recipeList.get(i).getName());
     }
 
     @Override
@@ -38,20 +43,19 @@ public class RecipeAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Recipe recipe = recipeList.get(position);
-
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        View view = inflater.inflate(R.layout.recipe_holder, parent,false);
-        TextView textView = view.findViewById(R.id.recipe_name);
-       // ImageView imageView = view.findViewById(R.id.recipe_image);
-
-        textView.setText(recipe.getName());
-       // imageView.setImageResource(recipe.getImage());
-
-        return view;
+    public int getItemCount() {
+        return recipeList.size();
     }
 
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView recipeTitle;
+
+        //   final ImageView recipeImage ;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeTitle = itemView.findViewById(R.id.recipe_title);
+
+        }
+    }
 }
